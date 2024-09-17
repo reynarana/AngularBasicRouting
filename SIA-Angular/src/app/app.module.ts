688462725fdf837444bd0ab512d437bb04ca4804
reynarana <reynalyncruzarana@gmail.com>
2024-09-17 20:00:10 +0800

    SIA-Angular

---
+++ SIA-Angular/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HomepageComponent } from "./components/homepage/homepage.component";
+import { LandingpageComponent } from "./components/landingpage/landingpage.component";
+import { LoginComponent } from "./components/login/login.component";
+import { SignupComponent } from "./components/signup/signup.component";
+
+const routes: Routes = [
+    {path: "", component: HomepageComponent},
+    {path: "landingpage", component: LandingpageComponent},
+    {path: "login", component: LoginComponent},
+    {path: "signup", component: SignupComponent}
+]
+
+@NgModule({
+
+    imports:[
+        CommonModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        RouterModule.forRoot(routes, {enableTracing:true}),
+    ],
+    declarations:[
+        AppComponent,
+        HomepageComponent,
+        LandingpageComponent,
+        LoginComponent,
+        SignupComponent,
+    ],
+    providers:[
+
+    ],
+    bootstrap:[
+        AppComponent
+    ]
+})
+
+export class AppModule{}
